f3f9389477391674e7bc0d9eb3c5a1e8202da207
Alexandre Garel <alexandre@Lennox.(none)>
2009-09-10 14:41:36 +0200

    better computation of tags size, fixed bug that was breaking object deletion

--- menhir/simple/tag/engine.py
+++ menhir/simple/tag/engine.py
@@ -13,8 +13,92 @@ class EngineUtility(grok.LocalUtility):
     """
     grok.provides(lovely.tag.interfaces.ITaggingEngine)
     
+    # make it editable attributes with a form
+    min_tag_size = 0.4
+    max_tag_size = 2
+    base_tag_size = 1 # the size for one tag alone eg.
+    expected_cloud_width = 20 # expected width in em
+    
+    def cloud(self, items):
+        """
+        compute tag info
+        
+        - giving respective weights
+        - ordering to have tags harmonously placed 
+        
+        
+        >>> class dummyEngine(object):
+        ...     def getCloud(self, items):
+        ...         return (("toto", 20),("tata", 10), ("titi", 5))
+        ... 
+        >>> a = EngineUtility()
+        >>> a.base_tag_size = 1
+        >>> a.max_tag_size = 5
+        >>> a.min_tag_size = .2
+        >>> a.expected_cloud_width = 10
+        
+        >>> setattr(a, "_engine", dummyEngine())
+        >>> a.cloud(0)
+        [('titi', 0.20000000000000001), ('toto', 3.0), ('tata', 1.1333333333333333)]
+
+        >>> class dummyEngine2(dummyEngine):
+        ...     def getCloud(self, items):
+        ...         return (("toto", 1),("tata", 1))
+        ... 
+        >>> setattr(a, "_engine", dummyEngine2())
+        >>> a.cloud(O)        
+        [('toto', 0.5), ('tata', 0.5)]
+        
+        FIXME :add test with empty sequence
+        FIXME review result values  as I changed computation
+        """
+        cloudInfo = self._engine.getCloud(items=items)
+        if cloudInfo:
+            # we do not always user min and max, we use those values
+            # only if difference between tags is big enough
+            minw = min(weight for tag, weight in cloudInfo)
+            maxw = max(weight for tag, weight  in cloudInfo)
+            balance = maxw / float(minw) - 1
+            stronger = min(self.base_tag_size + balance / 2,
+                             float(self.max_tag_size))
+            weaker = max(self.base_tag_size - balance / 2, 
+                             float( self.min_tag_size))
+            # now that a scale problem
+            if maxw == minw:
+                scale = 1
+            else:
+                scale = (stronger - weaker) / (maxw - minw)
+            relativeCloud = [(tag, (weight - minw) * scale + weaker) 
+                                for tag, weight in cloudInfo]
+            # ordering 
+            # get estimated width of each tag
+            widths = list((w * len(t), (t,w)) for t,w in relativeCloud)
+            # estimates how many lines we will need
+            lines_nb = sum(w for w, dummy in widths) / self.expected_cloud_width + 1
+            space = [self.expected_cloud_width ,] * int(lines_nb)
+            cloudbyline = [[] for i in range(int(lines_nb))]
+            # order by width
+            widths.sort(key = lambda e: e [0])
+            # add line by line, with line having more space
+            for width, (tag, weight) in widths:
+                index = space.index(max(space))
+                space[index] -= width
+                cloudbyline[index].append((tag, weight))
+            cloud = []
+            # append lines reverting one over two
+            transfo, nexttransfo = lambda x:x, reversed
+            for line in cloudbyline:
+                cloud.extend(line)
+                transfo, nexttransfo = nexttransfo, transfo
+        else:
+            cloud = []
+        return cloud
+    
+    
     def __init__(self, *args, **kwargs):
         self._engine = lovely.tag.TaggingEngine()
+        # put in context
+        setattr(self._engine, '__parent__', self)
     
     def __getattr__(self, name):
         # delegate all
@@ -26,4 +110,8 @@ def register_engine(ob, event):
     """Register utility (at site creation)
     """
     sm = getSiteManager(ob)
-    sm.registerUtility(EngineUtility(), lovely.tag.interfaces.ITaggingEngine)
+    e = EngineUtility()
+    sm.registerUtility(e, lovely.tag.interfaces.ITaggingEngine)
+    # set in context as lovely.tag needs it
+    e.__parent__ = ob
+    
--- menhir/simple/tag/views.py
+++ menhir/simple/tag/views.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import grokcore.viewlet as grok
+import megrok.z3cform.base as z3cform
 
 from zope.schema import TextLine
 from zope.app.intid.interfaces import IIntIds
-from zope.cachedescriptors.property import Lazy
 from zope.component import getUtility, getMultiAdapter
-from lovely.tag.interfaces import IUserTagging, ITaggingEngine, ITaggable
+from zope.cachedescriptors.property import Lazy
 
+from z3c.form.field import Field, Fields
 from dolmen.content import IBaseContent
-from dolmen.forms.base import Field, Fields, button
 from dolmen.app.layout import master, IDisplayView, Form
+from lovely.tag.interfaces import IUserTagging, ITaggingEngine, ITaggable
 
 grok.context(ITaggable)
 
@@ -20,6 +21,9 @@ class TagsViewlet(grok.Viewlet):
     grok.viewletmanager(master.DolmenTop)
     grok.require("dolmen.content.View")
     grok.order(100)
+    
+    def cloud(self):
+        return self.engine.cloud(items=(self.contextId,))
     
     def update(self):
         self.form = getMultiAdapter((self.context, self.request),
@@ -31,13 +35,12 @@ class TagsViewlet(grok.Viewlet):
         self.actual_url = str(self.request.URL)
         
         try:
-            cloudInfo = self.engine.getCloud(items=(self.contextId,))
             self.user_tags = IUserTagging(self.context).tags
             self.cloudInfo = [{
                 'tag': tag,
                 'weight': weight,
                 'marked': tag in self.user_tags
-                } for tag, weight in cloudInfo]
+                } for tag, weight in self.cloud()]
  
         except KeyError:
             self.user_tags = []
@@ -51,7 +54,23 @@ class TagsViewlet(grok.Viewlet):
     def contextId(self):
         intids = getUtility(IIntIds)
         return intids.getId(self.context)
-
+        
+#~ class CachedTagsViewlet(grok.Viewlet):
+    #~ """
+    #~ TagsViewlet caching tag cloud computation result in context
+    #~ 
+    #~ FIXME work in progress
+    #~ """
+    #~ 
+    #~ cache_key = "menhir_simple_tag_viewlet_cache"
+#~ 
+    #~ def cloud(self):
+        #~ if not hasattr(self.context, self.cache_key):
+            #~ setattr(self.context, self.cache_key,
+                    #~ super(self, CachedTagsViewlet).cloud())
+        #~ return getattr(self.context, self.cache_key)
+        #~ 
+    #~ # FIXME needs an event on tag update to remove cache
 
 class AddTag(Form):
     grok.name('user_tags_add')
@@ -63,7 +82,7 @@ class AddTag(Form):
         name = "tag"
         ))
     
-    @button.buttonAndHandler(u'Add', name='add')
+    @z3cform.button.buttonAndHandler(u'Add', name='add')
     def handleAdd(self, action):
         data, errors = self.extractData()
         if errors:
